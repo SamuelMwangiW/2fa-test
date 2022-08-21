7a7c621639ed7c69da9c2ac2a725fef6f7d3b909
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-08-22 02:56:21 +0300

    Added notification with mail option

---
+++ app/Notifications/SendOTP.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Actions\TwoFactor\GenerateOTP;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use PragmaRX\Google2FA\Exceptions\IncompatibleWithGoogleAuthenticatorException;
+use PragmaRX\Google2FA\Exceptions\InvalidCharactersException;
+use PragmaRX\Google2FA\Exceptions\SecretKeyTooShortException;
+
+class SendOTP extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function via(User $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(User $notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Your security code is '.$this->getTwoFactorCode($notifiable))
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    public function toArray(User $notifiable)
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * @throws IncompatibleWithGoogleAuthenticatorException
+     * @throws SecretKeyTooShortException
+     * @throws InvalidCharactersException
+     */
+    public function getTwoFactorCode(User $notifiable): ?string
+    {
+        if(!$notifiable->two_factor_secret){
+            return null;
+        }
+
+        return GenerateOTP::for(
+            decrypt($notifiable->two_factor_secret)
+        );
+    }
+}
