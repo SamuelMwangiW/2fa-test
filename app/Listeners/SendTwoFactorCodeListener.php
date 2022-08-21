01dea4b7b88d11c552ae66f6bdea63293a864af8
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-08-22 02:13:57 +0300

    Add SendTwoFactorCodeListener and register it

---
+++ app/Listeners/SendTwoFactorCodeListener.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Notifications\SendOTP;
+use Laravel\Fortify\Events\TwoFactorAuthenticationChallenged;
+use Laravel\Fortify\Events\TwoFactorAuthenticationEnabled;
+
+class SendTwoFactorCodeListener
+{
+    public function handle(
+        TwoFactorAuthenticationChallenged|TwoFactorAuthenticationEnabled $event
+    ): void {
+        $event->user->notify(new SendOTP());
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,41 +2,24 @@
 
 namespace App\Providers;
 
+use App\Listeners\SendTwoFactorCodeListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+use Laravel\Fortify\Events\TwoFactorAuthenticationChallenged;
+use Laravel\Fortify\Events\TwoFactorAuthenticationEnabled;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event to listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        TwoFactorAuthenticationChallenged::class => [
+            SendTwoFactorCodeListener::class,
+        ],
+        TwoFactorAuthenticationEnabled::class => [
+            SendTwoFactorCodeListener::class,
+        ],
     ];
-
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
-     * Determine if events and listeners should be automatically discovered.
-     *
-     * @return bool
-     */
-    public function shouldDiscoverEvents()
-    {
-        return false;
-    }
 }
